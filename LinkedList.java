9baaa209c96b9a66f5bc1c44e0370ed1e1d9a07c
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-06-06 17:24:24 +0530

    [Ranjith] modified LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,1 +1,12 @@
 import java.util.*;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		System.out.println(list);
+	}
+}
